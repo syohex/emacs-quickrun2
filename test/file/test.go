ac0271c9db1ee03992a5fccc8eb8d0c22b27eabd
Shohei YOSHIDA <syohex@gmail.com>
2020-03-21 22:44:07 +0900

    Add test files

---
+++ test/file/test.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello quickrun2 in golang")
+}
---
+++ test/file/test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+
+def main():
+    print("hello quickrun2 in python")
+
+
+if __name__ == '__main__':
+    main()
